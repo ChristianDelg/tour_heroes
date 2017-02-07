80de96f5ddbf916a37f6dfae811b0c5cde319bc7
Christian Delgado <ing.delgadomontiel@gmail.com>
2017-02-06 23:17:05 -0300

    holi

---
+++ app/app.component.ts
@@ -0,0 +1,9 @@
+/**
+ * Created by christian on 24/09/16.
+ */
+import { Component } from '@angular/core';
+@Component({
+    selector: 'my-app',
+    template: '<h1>Christian Marcelo Delgado Montiel</h1>'
+})
+export class AppComponent { }
---
+++ app/main.ts
@@ -0,0 +1,7 @@
+/**
+ * Created by christian on 24/09/16.
+ */
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { AppModule } from './app.module';
+const platform = platformBrowserDynamic();
+platform.bootstrapModule(AppModule);
